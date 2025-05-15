af8c507e1c5a28b652d1a9ff67c595d7161aa0c8
Semih Ayduran <semihayduran77@hotmail.com>
2025-05-15 13:27:31 +0300

    feat: icon_cinsiyet added

---
+++ lib/icon_cinsiyet.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:life_expectancy/container_widget.dart';
+
+
+class IconCinsiyet extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final Color? color;
+
+  const IconCinsiyet({super.key, required this.text, required this.icon, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return kutuYap(
+      color: color,
+      width: size.width * 0.445,
+      height: size.height*0.19,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 20,
+        children: [
+          Icon(icon, size: 50, color: Colors.black54),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
